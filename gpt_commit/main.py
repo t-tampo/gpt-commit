d520e4114865f8259e20e9e3b2920dd240ae2231
tkt-tmp <t.tampo@okorande.com>
2023-04-18 15:29:16 +0900

    main.pyにコメントを追加

--- gpt_commit/main.py
+++ gpt_commit/main.py
@@ -4,9 +4,17 @@ from gpt_commit.ai_model import generate_commit_message, handle_ai_exception
 from gpt_commit.utils import get_git_diff, commit_with_message, notify_commit_result
 
 def display_commit_message(commit_message: str) -> None:
+    """生成されたコミットメッセージを表示する。"""
     print(f"Generated commit message: {commit_message}")
 
 def get_user_choice(commit_message: str, allow_edit: bool) -> str:
+    """
+    ユーザーに生成されたコミットメッセージを使うか編集するか選んでもらう。
+
+    :param commit_message: 生成されたコミットメッセージ
+    :param allow_edit: True の場合、ユーザーはコミットメッセージを編集できる
+    :return: 最終的なコミットメッセージ。生成されたものか、編集されたもの
+    """
     user_input = input(f"Enter 'c' to use the generated commit message, or 'e' to edit it: ").lower()
     if allow_edit and user_input == 'e':
         return input("Enter your edited commit message: ")
@@ -16,22 +24,30 @@ def get_user_choice(commit_message: str, allow_edit: bool) -> str:
         raise ValueError("Invalid choice.")
 
 def main() -> None:
+    """AI によって生成されたコミットメッセージを使ってコミットを行うメイン関数。"""
+    # コマンドライン引数パーサーの設定
     parser = argparse.ArgumentParser(description="Generate a commit message with AI.")
     parser.add_argument('--no-edit', action='store_true', help="Disable editing of the generated commit message.")
     args = parser.parse_args()
 
+    # git diff を取得し、コミットメッセージを生成
     try:
         diff = get_git_diff()
         commit_message = generate_commit_message(diff)
     except Exception as e:
+        # 例外を処理し、ユーザーに手動でコミットメッセージを入力させる
         print("An error occurred while generating the commit message:")
         print(handle_ai_exception(e))
         commit_message = input("Please enter a commit message manually: ")
     else:
+        # 生成されたコミットメッセージを表示
         display_commit_message(commit_message)
+
+        # 編集が許可されている場合、ユーザーに生成されたコミットメッセージを使うか編集するか選んでもらう
         if not args.no_edit:
             commit_message = get_user_choice(commit_message, True)
 
+    # 選択されたコミットメッセージでコミットし、結果をユーザーに通知
     result = commit_with_message(commit_message)
     notify_commit_result(result)
 
