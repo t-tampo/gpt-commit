6958f53955c781ce4e94ffa250905a124c9c14fa
tkt-tmp <t.tampo@okorande.com>
2023-04-18 15:14:24 +0900

    GPT2Tokenizerを追加して、トークン数をカウントする関数を追加

--- gpt_commit/ai_model.py
+++ gpt_commit/ai_model.py
@@ -1,25 +1,54 @@
 import random
 from typing import List
+from transformers import GPT2Tokenizer
+import openai
+
+from langchain import PromptTemplate, OpenAI, LLMChain
+
+
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+def count_tokens(example_string: str) -> int:
+    tokens = tokenizer.tokenize(example_string)
+    return len(tokens)
 
 class DummyAIModel:
-    # このクラスは、ダミーのAIモデルを示しています。適切なAIモデルに置き換えてください。
+    def generate_message(self, diff: str) -> str:
+        template = """
+        Instruction:
+        Generate 1 appropriate commit messages using the following diff.
+        The output commit message must be composed of a single line.
+        Additionally, the commit messages must be in the following format.
+        output language: Japanese
 
-    def generate_messages(self, diff: str) -> List[str]:
-        dummy_messages = [
-            "Update README.md",
-            "Fix a bug in the main function",
-            "Refactor code for better readability",
-            "Add new feature to the application"
-        ]
-        return random.sample(dummy_messages, len(dummy_messages))
+        Diff:
+        {diff}
 
-def generate_commit_messages(diff: str) -> List[str]:
+        Output:
+        """
+        prompt = PromptTemplate(template=template, input_variables=["diff"])
+        llm_chain = LLMChain(
+            llm=OpenAI(temperature=0), 
+            prompt=prompt, 
+            # verbose=True
+        )
+
+        # もし差分が4000トークンを超えていたら、エラーを返す
+        if count_tokens(diff) > 4000:
+            raise Exception("The diff is too long.")
+        
+        message = llm_chain.predict(diff=diff)
+
+        return message
+
+def generate_commit_message(diff: str) -> str:
     ai_model = DummyAIModel()
     try:
-        commit_messages = ai_model.generate_messages(diff)
+        commit_message = ai_model.generate_message(diff)
     except Exception as e:
         raise e
-    return commit_messages
+    return commit_message
 
 def handle_ai_exception(e: Exception) -> str:
     error_message = f"An error occurred while generating commit messages: {str(e)}"
--- gpt_commit/main.py
+++ gpt_commit/main.py
@@ -1,40 +1,40 @@
 import argparse
 from typing import List
 
-from gpt_commit.ai_model import generate_commit_messages, handle_ai_exception
+from gpt_commit.ai_model import generate_commit_message, handle_ai_exception
 from gpt_commit.utils import get_git_diff, commit_with_message, notify_commit_result
 
-def display_commit_messages(commit_messages: List[str]) -> None:
-    for idx, msg in enumerate(commit_messages, start=1):
-        print(f"{idx}: {msg}")
+def display_commit_message(commit_message: str) -> None:
+    print(f"Generated commit message: {commit_message}")
 
-def get_user_choice(commit_messages: List[str], allow_edit: bool) -> str:
-    user_input = input(f"Enter the number of your preferred commit message{( ', or e to edit' if allow_edit else '')}: ")
-    if allow_edit and user_input.lower() == 'e':
+def get_user_choice(commit_message: str, allow_edit: bool) -> str:
+    user_input = input(f"Enter 'c' to use the generated commit message, or 'e' to edit it: ").lower()
+    if allow_edit and user_input == 'e':
         return input("Enter your edited commit message: ")
-
-    choice = int(user_input) - 1
-    if 0 <= choice < len(commit_messages):
-        return commit_messages[choice]
+    elif user_input == 'c':
+        return commit_message
     else:
         raise ValueError("Invalid choice.")
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Generate commit messages with AI.")
-    parser.add_argument('--edit', action='store_true', help="Allow editing of generated commit messages.")
+    parser = argparse.ArgumentParser(description="Generate a commit message with AI.")
+    parser.add_argument('--edit', action='store_true', help="Allow editing of the generated commit message.")
     args = parser.parse_args()
 
     try:
         diff = get_git_diff()
-        commit_messages = generate_commit_messages(diff)
+        commit_message = generate_commit_message(diff)
+        print("-------------------------")
+        print(commit_message)
+        print("-------------------------")
     except Exception as e:
-        print("An error occurred while generating commit messages:")
+        print("An error occurred while generating the commit message:")
         print(handle_ai_exception(e))
         commit_message = input("Please enter a commit message manually: ")
     else:
-        print("Generated commit messages:")
-        display_commit_messages(commit_messages)
-        commit_message = get_user_choice(commit_messages, args.edit)
+        display_commit_message(commit_message)
+        if args.edit:
+            commit_message = get_user_choice(commit_message, True)
 
     result = commit_with_message(commit_message)
     notify_commit_result(result)
