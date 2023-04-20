a496d9e399ec3398e62de7e2bf103d22fbf9b9c6
tkt-tmp <t.tampo@okorande.com>
2023-04-20 12:10:50 +0900

    機能を追加してプロンプトツールキットをインストールするようにpyproject.tomlとpoetry.lockを更新した

--- gpt_commit/main.py
+++ gpt_commit/main.py
@@ -1,4 +1,8 @@
 import argparse
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
 
 from gpt_commit.ai_model import generate_commit_message, handle_ai_exception
 from gpt_commit.utils import GitUtils
@@ -13,7 +17,18 @@ def get_user_choice(commit_message: str, allow_edit: bool) -> str:
     user_input = input("Enter 'c' to confirm and use the generated commit message, 'e' to edit the message, or 'q' to quit the program: ").lower()
 
     if allow_edit and user_input == 'e':
-        return input("Enter your edited commit message: ")
+        session = PromptSession(history=InMemoryHistory())
+        bindings = KeyBindings()
+
+        @bindings.add(Keys.ControlD)
+        def _(event):
+            event.app.exit()
+
+        try:
+            edited_message = session.prompt("Edit the generated commit message (press Enter to keep the original, Ctrl-D to finish editing):\n", default=commit_message, multiline=True, key_bindings=bindings)
+        except KeyboardInterrupt:
+            return commit_message.strip()
+        return edited_message.strip() if edited_message and edited_message.strip() else commit_message
     elif user_input == 'c':
         return commit_message
     elif user_input == 'q':
@@ -30,7 +45,7 @@ def main() -> None:
     try:
         print("Getting git diff...")
         diff = GitUtils.get_git_diff()
-        
+
         if not diff:
             print("No changes to commit.")
             exit(0)
